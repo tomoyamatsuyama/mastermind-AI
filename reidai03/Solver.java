4e3327c9851a5c542d5f6655ebd7b3cbf0abb892
Tomoya Matsuyama <m.tomoya0221@gmail.com>
2018-05-18 13:42:03 +0900

    Add Solver

--- reidai03/Solver.java
+++ reidai03/Solver.java
@@ -1,34 +1,40 @@
-import java.util.Random;
+import java.util.*;
 
 public class Solver {
-	static int limit = MasterMind.getlimit();
-	static int zigen = MasterMind.getzigen();
+	static int limit = MasterMind.getlimit(); // 提出回数
+	static int zigen = MasterMind.getzigen(); // 正解の桁数
 	static int[] x = new int[zigen];
 	static int[] hint = new int[2];
-	static int[] bestanswer = new int[zigen];
-	static int[] bestscore = { 0, 0 };
+	static ArrayList<Integer> search = new ArrayList<Integer>(); //ここに仮のやつを入れる。そのあとに全探索
 
 	public static void answer() {
-		Random r = new Random();
-		for (int i=0; i<limit; i++) {
+		for(int i=0; i<=9; i++) {
 			for (int j=0; j<zigen; j++) {
-				x[j] = r.nextInt(9);
+				x[j] = i;
+			}
+
+			hint = MasterMind.evaluate(x);
+
+			if (hint[0] > 0) {
+				for(int h=0; h<hint[0]; h++) {
+					search.add(i);
+				}
+			}
+		}
+
+		Permutation per = new Permutation();
+		List<List<Integer>> lists = per.make(search);
+
+		for(List<Integer> li: lists) {
+			for(int l=0; l<zigen;l++) {
+				x[l] = li.get(l);
 			}
 			hint = MasterMind.evaluate(x);
-			if(bestscore[0] < hint[0]){
-				update();
-			} else if (bestscore[1] < hint[1]){
-				update();
+
+			if(hint[0] == zigen) {
+				MasterMind.submit(x);
+				break;
 			}
 		}
-		MasterMind.submit(bestanswer);
-	}
-
-	private static void update() {
-		for (int j=0; j<zigen; j++) {
-			bestanswer[j] = x[j];
-		}
-		bestscore[0] = hint[0];
-		bestscore[1] = hint[1];
 	}
 }
